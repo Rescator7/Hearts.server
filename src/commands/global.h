0b4b240a891bb61464e3cb626c9a4fb589334ba0
Rescator7 <castillo7@hotmail.com>
2020-03-20 09:20:04 -0400

    Delete global.h

--- src/commands/global.h
+++
@@ -1,8 +0,0 @@
-#ifndef _GLOBAL_
-#define _GLOBAL_
-extern bool server_shutdown;
-extern bool server_shutoff;
-extern unsigned int num_table;
-extern class cDescList *descriptor_list;
-extern class cTabList *table_list;
-#endif
