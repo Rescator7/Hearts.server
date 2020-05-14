947468bb9253fcd3c37416752addfd759324cd88
Rescator7 <castillo7@hotmail.com>
2020-05-14 15:44:03 -0400

    Delete help.cpp

--- commands/help.cpp
+++
@@ -1,9 +0,0 @@
-#include "../define.h"
-#include "../datagrams.h"
-#include "../global.h"
-#include "help.h"
-
-void cHelp::Execute( cDescriptor &d, cParam &param )
-{
-  cmd.Help(d, param.arguments);
-}
